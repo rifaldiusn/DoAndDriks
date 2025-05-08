2aff80377ea3f0a67598b0ff3f7956bd469f1295
NararyaPutra <gungwah62@gmail.com>
2025-05-08 07:49:36 +0700

    tambahan controller minuman

--- src/main/java/com/example/websiteminuman/controller/MinumanController.java
+++ src/main/java/com/example/websiteminuman/controller/MinumanController.java
@@ -1,5 +1,20 @@
 package com.example.websiteminuman.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.websiteminuman.entities.Minuman;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class MinumanController {
+    @GetMapping("/")
+    public List<Minuman> getAllMinuman() {
+        return null;
+    }
     
 }
--- src/main/java/com/example/websiteminuman/repositories/MinumanRepository.java
+++ src/main/java/com/example/websiteminuman/repositories/MinumanRepository.java
@@ -1,7 +1,5 @@
-// package com.example.websiteminuman.repositories;
+package com.example.websiteminuman.repositories;
 
-// import com.example.websiteminuman.model.Minuman;
-// import org.springframework.data.jpa.repository.JpaRepository;
+public interface MinumanRepository {
 
-// public interface MinumanRepository extends JpaRepository<Minuman, Long> {
-// }
+}
